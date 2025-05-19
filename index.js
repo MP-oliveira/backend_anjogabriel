b310cfcf1771e07257df20a82bd83ccd9acb5197
Mauricio Oliveira <mau_oliver@hotmail.com>
2025-05-19 12:04:18 -0300

    fix: adjustin backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const adminRoutes = require('./routes/adminRoutes');
 const alunoRoutes = require('./routes/alunoRoutes');
 const cursoRoutes = require('./routes/cursoRoutes');
